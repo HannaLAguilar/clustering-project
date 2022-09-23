068a2135b908c88a6e69cd68649b7e96fd77b7df
hlaguilar <hanna.laguilar@gmail.com>
2022-09-23 17:35:20 +0200

    add data and import_data func

---
+++ clustering/data/preprocessing.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+import pandas as pd
+from pathlib import Path
+from scipy.io import arff
+
+from clustering.path_definitions import RAW_DATA_PATH
+
+
+def import_data(p: Path) -> Tuple[pd.DataFrame,
+                                  arff.arffread.MetaData]:
+    data, meta = arff.loadarff(p)
+    df = pd.DataFrame(data)
+    return df, meta
+
+
+def check_null_values(df: pd.DataFrame) -> pd.Series:
+    return df.isnull().sum()
+
+
+# Explore
+path_file = RAW_DATA_PATH / 'heart-c.arff'
+df, meta = import_data(path_file)
+nulls = check_null_values(df)
---
+++ clustering/path_definitions.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent.parent
+DATA_PATH = ROOT_PATH / 'data'
+RAW_DATA_PATH = DATA_PATH / 'raw'
