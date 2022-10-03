0a28d6becf5f36a03d81a042f08465b165e3d2d4
hlaguilar <hlaguilar@oktics.com>
2022-10-03 18:49:35 +0200

    update datasets

--- clustering/data/preprocessing.py
+++ clustering/data/preprocessing.py
@@ -5,6 +5,8 @@ from scipy.io import arff
 
 from clustering.path_definitions import RAW_DATA_PATH
 
+import cv2
+
 
 def import_data(p: Path) -> Tuple[pd.DataFrame,
                                   arff.arffread.MetaData]:
@@ -18,6 +20,6 @@ def check_null_values(df: pd.DataFrame) -> pd.Series:
 
 
 # Explore
-path_file = RAW_DATA_PATH / 'heart-c.arff'
+path_file = RAW_DATA_PATH / 'audiology.arff'
 df, meta = import_data(path_file)
 nulls = check_null_values(df)
