561787e9275c8cfbec3c1cae66493f9f08557379
hlaguilar <hlaguilar@oktics.com>
2022-10-10 14:03:15 +0200

    make preprocessing

--- clustering/data/preprocessing.py
+++ clustering/data/preprocessing.py
@@ -1,25 +1,75 @@
-from typing import Tuple
+from typing import Tuple, Union
+import numpy as np
 import pandas as pd
 from pathlib import Path
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
 from scipy.io import arff
 
-from clustering.path_definitions import RAW_DATA_PATH
-
-import cv2
+from clustering.path_definitions import RAW_DATA_PATH, PROCESSED_DATA_PATH
 
 
-def import_data(p: Path) -> Tuple[pd.DataFrame,
-                                  arff.arffread.MetaData]:
+def import_data(p: Union[Path, str]) -> Tuple[pd.DataFrame,
+                                              arff.arffread.MetaData]:
     data, meta = arff.loadarff(p)
     df = pd.DataFrame(data)
     return df, meta
+
+
+def remove_predicted_value(df: pd.DataFrame) -> pd.DataFrame:
+    return df.iloc[:, :-1]
 
 
 def check_null_values(df: pd.DataFrame) -> pd.Series:
     return df.isnull().sum()
 
 
-# Explore
-path_file = RAW_DATA_PATH / 'audiology.arff'
-df, meta = import_data(path_file)
-nulls = check_null_values(df)
+def standardization(df: pd.DataFrame) -> np.ndarray:
+    try:
+        df['ca'] = df['ca'].astype('object')  # Only for heart-c dataset
+    except:
+        pass
+    num_features = df.select_dtypes(include=np.number).columns
+    num_transformer = Pipeline(steps=[
+        ('replace_nan', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler())])
+    cat_features = df.select_dtypes(exclude=np.number).columns
+    cat_transformer = Pipeline(steps=[
+        ('replace_nan', SimpleImputer(strategy='most_frequent')),
+        ('encoder', OrdinalEncoder())])
+
+    ct = ColumnTransformer(
+        transformers=[
+            ('num', num_transformer, num_features),
+            ('cat', cat_transformer, cat_features)])
+
+    X_trans = ct.fit_transform(df)
+    return X_trans
+
+
+def preprocessing(data_name: str, save: bool = True) -> pd.DataFrame:
+    # Preprocessing
+    print(f'---Preprocessing {data_name} dataset---')
+    data_path = RAW_DATA_PATH / data_name
+    df, meta = import_data(data_path)
+    df = remove_predicted_value(df)
+    columns = df.columns
+    nulls = check_null_values(df)
+    if nulls.sum() != 0:
+        print(f'There is nulls values: {nulls}')
+    else:
+        print(f'Nan values: 0')
+
+    X = standardization(df)
+    # Save
+    if save:
+        df = pd.DataFrame(X, columns=columns)
+        df.to_csv(PROCESSED_DATA_PATH / data_name)
+    return df
+
+
+if __name__ == '__main__':
+    DATASET_PATH = ['heart-c.arff', 'wine.arff', 'audiology.arff']
+    PROCESSED_DATASETS = [preprocessing(name) for name in DATASET_PATH]
--- clustering/path_definitions.py
+++ clustering/path_definitions.py
@@ -3,3 +3,4 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent
 DATA_PATH = ROOT_PATH / 'data'
 RAW_DATA_PATH = DATA_PATH / 'raw'
+PROCESSED_DATA_PATH = DATA_PATH / 'processed'
