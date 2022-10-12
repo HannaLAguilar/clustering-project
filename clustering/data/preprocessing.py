ec03d2b04e7730faa12f7f7c8631bba03af65320
hlaguilar <hlaguilar@oktics.com>
2022-10-12 13:19:57 +0200

    update preprocessing code

--- clustering/data/preprocessing.py
+++ clustering/data/preprocessing.py
@@ -11,10 +11,18 @@ from scipy.io import arff
 from clustering.path_definitions import RAW_DATA_PATH, PROCESSED_DATA_PATH
 
 
+def string_decode(df: pd.DataFrame):
+    for col in df.columns:
+        if df[col].dtype == object:
+            df[col] = df[col].str.decode('utf-8')
+    return df
+
+
 def import_data(p: Union[Path, str]) -> Tuple[pd.DataFrame,
                                               arff.arffread.MetaData]:
     data, meta = arff.loadarff(p)
     df = pd.DataFrame(data)
+    df = string_decode(df)
     return df, meta
 
 
@@ -26,27 +34,50 @@ def check_null_values(df: pd.DataFrame) -> pd.Series:
     return df.isnull().sum()
 
 
-def standardization(df: pd.DataFrame) -> np.ndarray:
-    try:
-        df['ca'] = df['ca'].astype('object')  # Only for heart-c dataset
-    except:
-        pass
+def standardization(df: pd.DataFrame, columns=None) -> pd.DataFrame:
+    # numerical features
     num_features = df.select_dtypes(include=np.number).columns
     num_transformer = Pipeline(steps=[
         ('replace_nan', SimpleImputer(strategy='mean')),
         ('scaler', StandardScaler())])
+    # categorical features
     cat_features = df.select_dtypes(exclude=np.number).columns
     cat_transformer = Pipeline(steps=[
         ('replace_nan', SimpleImputer(strategy='most_frequent')),
         ('encoder', OneHotEncoder())])
 
+    # transform columns
     ct = ColumnTransformer(
         transformers=[
             ('num', num_transformer, num_features),
             ('cat', cat_transformer, cat_features)])
-
     X_trans = ct.fit_transform(df)
-    return X_trans
+
+    # dataset cases
+    # case 1: categorical and numerical features
+    if len(cat_features) != 0 and len(num_features) != 0:
+        columns_encoder = ct.transformers_[1][1]['encoder'].\
+            get_feature_names_out(cat_features)
+        columns = num_features.union(pd.Index(columns_encoder), sort=False)
+
+    # case 2: only categorical features
+    elif len(cat_features) != 0 and len(num_features) == 0:
+        columns = ct.transformers_[1][1]['encoder'].\
+            get_feature_names_out(cat_features)
+        columns = pd.Index(columns)
+        X_trans = X_trans.toarray()
+
+    # case 3: only numerical features
+    elif len(cat_features) == 0 and len(num_features) != 0:
+        columns = num_features
+
+    # catch an error
+    else:
+        print('There is a problem with features')
+
+    # processed dataset
+    processed_df = pd.DataFrame(X_trans, columns=columns)
+    return processed_df
 
 
 def preprocessing(data_name: str, save: bool = True) -> pd.DataFrame:
@@ -55,21 +86,19 @@ def preprocessing(data_name: str, save: bool = True) -> pd.DataFrame:
     data_path = RAW_DATA_PATH / data_name
     df, meta = import_data(data_path)
     df = remove_predicted_value(df)
-    columns = df.columns
     nulls = check_null_values(df)
     if nulls.sum() != 0:
         print(f'There is nulls values: {nulls}')
     else:
         print(f'Nan values: 0')
 
-    X = standardization(df)
+    process_df = standardization(df)
     # Save
     if save:
-        df = pd.DataFrame(X, columns=columns)
-        df.to_csv(PROCESSED_DATA_PATH / data_name)
-    return df
+        process_df.to_csv(PROCESSED_DATA_PATH / data_name)
+    return process_df
 
 
 if __name__ == '__main__':
-    DATASET_PATH = ['vowel.arff', 'pen-based.arff', 'mushroom.arff']
+    DATASET_PATH = ['mushroom.arff', 'vowel.arff', 'pen-based.arff', ]
     PROCESSED_DATASETS = [preprocessing(name) for name in DATASET_PATH]
